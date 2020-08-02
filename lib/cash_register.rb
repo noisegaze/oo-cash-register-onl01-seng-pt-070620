51168740df5e763791e487e83c842fb6a7121b26
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-02 17:51:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
     if discount == 0 
       "There is no discount to apply"
     else discount  > 0 
-      self.total -= discount
+      elf.discount -= %discount
       "After the discount, your total is #{self.total}."
     end  
   end
