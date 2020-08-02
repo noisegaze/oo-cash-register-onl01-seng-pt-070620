65107bc1f809a5958834623e6dada23c60345615
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-02 18:36:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,13 +11,8 @@ class CashRegister
   end 
   
   def add_item(title,price,quantity=1)
-    if quantity <= 1 
-         self.total += price 
-      elsif quantity > 1 
-        self.total += price*quantity
-      else 
-        nil 
-    end
+    self.total += price*quantity
+    @items << 
   end
   
   def apply_discount
