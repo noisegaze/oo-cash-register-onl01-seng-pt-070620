543357a78bc5e5d4b5c7d6b4564dcdf817e7f7f0
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-02 17:14:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,56 @@
+require 'pry'
 
+class CashRegister 
+  
+  attr_accessor :total,:discount
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount 
+  end 
+  
+  def add_item(title,price,quantity=1)
+    if quantity <= 1 
+         self.total += price 
+      elsif quantity > 1 
+        self.total += price*quantity
+      else 
+        nil 
+    end
+  end
+  
+  def apply_discount
+    self.total -= @discount
+    puts "After the discount, your total is #{self.total}"
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
