55e83c11732f73ee4a113fa0d6b331a16d0e3c53
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-02 19:56:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -29,7 +29,7 @@ class CashRegister
   end
   
   def void_the_last_transaction
-    self.total -= @last_transaction_price*@items.pop
+    self.total -= @last_transaction_price*@last_transaction_quantity
   end 
   
   
